0a30dac6983bb69ad128e47241767538b3c2be4b
whydandrian <whydandrian@gmail.com>
2020-11-23 12:35:23 +0700

    add bootstrap assets

--- apps/Codeigniter-v3/application/views/Home.php
+++ apps/Codeigniter-v3/application/views/Home.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TEST - PROGRAMMER 04 PT INTALOGI</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
 </head>
 
 <body>
 
+    <script src="<?= base_url('assets/bootstrap/js/bootstrap.min.js') ?>"></script>
 </body>
 
 </html>
