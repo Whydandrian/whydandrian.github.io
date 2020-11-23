0de4d5a01f03feba3fc2df123b254c757ea8eb48
whydandrian <whydandrian@gmail.com>
2020-11-23 15:45:34 +0700

    add tes program

--- apps/Codeigniter-v3/application/controllers/Home.php
+++ apps/Codeigniter-v3/application/controllers/Home.php
@@ -12,6 +12,24 @@ class Home extends CI_Controller
 
 	public function index()
 	{
-		$this->load->view('Home');
+		$input = "informatika jogjakarta";
+		$Uppercase = strtoupper($input);
+		$convert = explode(" ", $Uppercase);
+		$kata = implode("", $convert);
+
+		foreach (count_chars($kata, 1) as $res => $val) {
+			if ($val > 1) {
+				$x = chr($res);
+				$x .= $val;
+				echo $x;
+			}
+		}
+		foreach (count_chars($kata, 1) as $res => $val) {
+			if ($val == 1) {
+				$y = chr($res);
+				echo $y;
+			}
+		}
+		return $this->load->view('home');
 	}
 }
--- apps/Codeigniter-v3/application/views/Home.php
+++ apps/Codeigniter-v3/application/views/Home.php
@@ -10,6 +10,35 @@
 
 <body>
 
+    <div class="container">
+        <div class="row mt-3">
+            <h2 class="text-center">Tes Logika - Programmer 04</h2>
+        </div>
+        <div class="row text-dark mt-4" style="height: 10rem;">
+            <div class="col-6 col-md-5 bg-light">
+                <h4 class="mt-3 ml-2 mb-2">Masukkan Kata</h4>
+                <form class="mt-3 ml-2" method="POST" action="<?= base_url() ?>">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="kata" name="kata" placeholder="Masukkan kata yang akan dihitung">
+                    </div>
+                    <button type="submit" class="btn btn-primary mb-4">Hitung huruf</button>
+                </form>
+            </div>
+            <div class="col-md-7">
+                <div class="card border-dark" style="height: 11rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Hasil Perhitungan</h5>
+                        <p class="card-text">
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="<?= base_url('assets/bootstrap/js/jquery-3.5.1.min.js') ?>"></script>
     <script src="<?= base_url('assets/bootstrap/js/bootstrap.min.js') ?>"></script>
 </body>
 
