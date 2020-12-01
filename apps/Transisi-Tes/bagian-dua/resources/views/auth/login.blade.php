02d01eb36d05a4411b38414180924e3c398f137b
whydandrian <whydandrian@gmail.com>
2020-12-01 19:51:49 +0700

    ubah textbox password

--- apps/Transisi-Tes/bagian-dua/resources/views/auth/login.blade.php
+++ apps/Transisi-Tes/bagian-dua/resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
+                        {{-- <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
@@ -23,13 +23,27 @@
                                     </span>
                                 @enderror
                             </div>
+                        </div> --}}
+
+                        <div class="form-group row">
+                            <label for="username" class="col-md-4 col-form-label text-md-right">Username or Email</label>
+                           
+                            <div class="col-md-6">
+                                <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required  autofocus placeholder="Username or Email">
+                           
+                                @error('username')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
                         </div>
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="********">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
