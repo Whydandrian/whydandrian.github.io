554a72d922115abc44d3a2d4423f6517d99812fb
whydandrian <whydandrian@gmail.com>
2020-12-01 20:58:35 +0700

    create seed role and user

---
+++ apps/Transisi-Tes/bagian-dua/database/seeds/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = User::create([
+            'name' => 'Administrator',
+            'email' => 'admin@transisi.id',
+            'password' => bcrypt('transisi'),
+        ]);
+
+        $admin->assignRole('administrator');
+    }
+}
