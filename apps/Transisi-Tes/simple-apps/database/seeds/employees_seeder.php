7f7a25b051aa79721bffdfc20b2671bf2bf1b0a3
whydandrian <whydandrian@gmail.com>
2020-12-06 20:08:58 +0700

    add migration & seeder companies,employees

---
+++ apps/Transisi-Tes/simple-apps/database/seeds/employees_seeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class employees_seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
--- apps/Transisi-Tes/simple-apps/resources/views/layouts/app.blade.php
+++ apps/Transisi-Tes/simple-apps/resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{-- {{ config('app.name', 'Laravel') }} --}}
-                    <img src="{{ asset('/storage/logo-transisi.jpg') }}" alt="">
+                    <img src="{{ asset('/storage/transisi.png') }}" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
